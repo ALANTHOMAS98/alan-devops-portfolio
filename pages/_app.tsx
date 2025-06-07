e8c3358c5c6b64c01d114f36b60085e6c4650b4c
ALANTHOMAS98 <alanthoma98@gmail.com>
2025-06-07 18:56:52 +0300

    Fix FaAward import to curly braces style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 // pages/_app.tsx
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from '../components/Navbar'   // <-- Add this line
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />  {/* Show navbar on every page */}
+      <Component {...pageProps} />
+    </>
+  )
 }
 
